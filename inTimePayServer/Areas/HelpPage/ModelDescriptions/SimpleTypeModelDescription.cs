f77268bbbc6c5163673a61d8d1137c73f11c0d59
jimmy <playstation.gui@gmail.com>
2014-09-19 16:51:19 +0800

    clean up the old demo

--- inTimePayServer/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace inTimePayServer.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- inTimePayServer/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="inTimePayServer.WebApiApplication" Language="C#" %>
